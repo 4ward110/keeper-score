d132094628853934f522a8973c74bd87bbf19024
long.vu <vuduclong10a1cma@gmail.com>
2019-10-25 00:50:31 +0700

    done

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 const shortid = require('shortid');
 const pug = require('pug');
-var db = require('./db.js');
+// var db = require('./db.js');
+const fs = require('fs');
 const port = 8080;
 
 const app = express();
@@ -17,14 +18,18 @@ app.set('view engine','pug');
 app.set('views','./views');
 
 app.get('/:gameId', (req,res) => {
-    var id = req.params.gameId;
-    var game = db.get("games").find({ id: id}).value();
-    console.log(game);
-    console.log(game.score);
+    var id1 = req.params.gameId;
+    // var game = db.get("games").find({ id: id}).value();
+
+    const fileData = fs.readFileSync("db.json", {encoding:"utf-8"});
+    const dataList = JSON.parse(fileData);
+    var currentGame = dataList['games'].find( ({id}) => id === id1 );
+    // var score = currentGame.score[number-1];
     res.render('scoreboard.pug',{
-        game: game,
-        score: game.score,
-        id: id
+        game: currentGame,
+        score: currentGame.score,
+        id: id1,
+        totalScore: currentGame.totalScore
     });
 })
 
@@ -37,11 +42,60 @@ app.post('/add-player', (req,res) => {
     var genrateId = shortid.generate();
     req.body.id = genrateId;
     req.body.score = [];
-    res.cookie('gameid',genrateId);    
-    db.get("games").push(req.body).write();
-    res.redirect(`/${genrateId}`);
-})
+    res.cookie('gameid',genrateId);
+    req.body.totalScore = [0,0,0,0];    
+    // db.get("games").push(req.body).write();
 
+    const fileData = fs.readFileSync("db.json", {encoding:"utf-8"});
+    const dataList = JSON.parse(fileData);
+    dataList['games'].push(req.body)
+    fs.writeFileSync('db.json',JSON.stringify(dataList));
+    res.redirect(`/${genrateId}`);
+});
+app.post('/updateScore', (req,res) => {
+    var gameId = req.cookies.gameid;
+    const fileData = fs.readFileSync("db.json", {encoding:"utf-8"});
+    const dataList = JSON.parse(fileData);
+    var currentGame = dataList['games'].find( ({id}) => id === gameId );
+
+    // var score = db.get("games").find({ id: gameId}).get("score").value();
+    currentGame.score.push([0,0,0,0]);
+    fs.writeFileSync('db.json',JSON.stringify(dataList));
+    // db.get("games").find({ id: gameId}).get("score").push([0,0,0,0]).write();
+    res.send({ score: currentGame.score });
+});
+
+app.post('/changeScore', (req,res) => {
+    var p1Score = 0;
+    var p2Score = 0;
+    var p3Score = 0;
+    var p4Score = 0;
+    var data = req.body.value;
+    var number = req.body.name;
+    var index = req.body.id;
+    var gameId = req.cookies.gameid;
+    const fileData = fs.readFileSync("db.json", {encoding:"utf-8"});
+    const dataList = JSON.parse(fileData);
+    var currentGame = dataList['games'].find( ({id}) => id === gameId );
+    var score = currentGame.score[number-1];
+    score[index] = parseInt(data);
+    // console.log(data,number,index);
+    fs.writeFileSync('db.json',JSON.stringify(dataList));
+    var allScoreData = currentGame.score;
+    for (let i = 0; i< allScoreData.length; i++) {
+        p1Score = p1Score + allScoreData[i][0];
+        p2Score = p2Score + allScoreData[i][1];
+        p3Score = p3Score + allScoreData[i][2];
+        p4Score = p4Score + allScoreData[i][3];
+    }
+    var saveScore = currentGame.totalScore;
+    saveScore[0] = p1Score;
+    saveScore[1] = p2Score;
+    saveScore[2] = p3Score;
+    saveScore[3] = p4Score;
+    fs.writeFileSync('db.json',JSON.stringify(dataList));
+    res.send({ totalScore: saveScore });
+})
 app.listen(port,() => {
     console.log('success!!');
 })
--- views/add.row.js
+++ views/add.row.js
@@ -1,12 +1,58 @@
+var changeValue = document.getElementsByTagName("input");
+for (let i = 0; i < changeValue.length; i++){
+    changeValue[i].addEventListener("input",() => {
+        $.ajax({
+            url:'/changeScore',
+            method: "POST",
+            contentType:"application/json",
+            data: JSON.stringify({ value: changeValue[i].value, 
+                                   name: changeValue[i].name, 
+                                   id: changeValue[i].id}),
+            success: (response) => {
+                var allElement = document.querySelectorAll("th#score");
+                for( let i = 0; i< response.totalScore.length; i++) {
+                    allElement[i].innerHTML = response.totalScore[i];
+                }
+            }
+        })
+    });
+}
 
 var button = document.getElementById('addRow');
-button.addEventListener("click", () => {
-    var newScoreBox = document.createElement('tr');
-    newScoreBox.innerHTML = "<th scope='row'>1</th>"+
-    `<td><input type='number' value='0'/></td>`+
-    "<td><input type='number' value='0'/></td>"+
-    "<td><input type='number' value='0'/></td>"+
-    "<td><input type='number' value='0'/></td>"
-    document.getElementById("body").appendChild(newScoreBox);
-    console.log(document.cookie)
+button.addEventListener(`click`, () => {
+    $.ajax({
+        url:'/updateScore',
+        method: 'POST',
+        success: function(response) {
+            var allScore = response.score;
+            var round = allScore[allScore.length-1];
+            var newScoreBox = document.createElement('tr');
+            newScoreBox.innerHTML = `<th scope='row'>${allScore.length}</th>`+
+            `<td><input id="0" type='number' name="${allScore.length}" value="${round[0]}"/></td>`+
+            `<td><input id="1" type='number' name="${allScore.length}" value="${round[1]}"/></td>`+
+            `<td><input id="2" type='number' name="${allScore.length}" value="${round[2]}"/></td>`+
+            `<td><input id="3" type='number' name="${allScore.length}" value="${round[3]}"/></td>`
+            document.getElementById("body").appendChild(newScoreBox);
+            
+            var changeValue = document.getElementsByTagName("input");
+            for (let i = 0; i < changeValue.length; i++){
+                changeValue[i].addEventListener("input",() => {
+                    $.ajax({
+                        url:'/changeScore',
+                        method: "POST",
+                        contentType:"application/json",
+                        data: JSON.stringify({ value: changeValue[i].value, 
+                                            name: changeValue[i].name, 
+                                            id: changeValue[i].id}),
+                        success: (response) => {
+                            var allElement = document.querySelectorAll("th#score");
+                            for( let i = 0; i< response.totalScore.length; i++) {
+                                allElement[i].innerHTML = response.totalScore[i];
+                            }
+                        }
+                    })
+                });
+            }
+        }
+    })
 });
